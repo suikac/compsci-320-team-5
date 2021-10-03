9de20b3336d167ac82ec134ded71181837c22474
James Topa <jtopa@gmail.com>
2021-10-03 18:39:24 -0400

    work in progress

---
+++ frontend/src/Login.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import logo from "./Logo2.png";
+class Login extends Component {
+    render() {
+        return (
+            <div class = "container">
+                <form>
+                    <h1>Login</h1>
+
+                    <div class="credentials">
+                        <label>Username</label>
+                        <input type="email" class="form-control" placeholder="Enter email" />
+                    </div>
+
+                    <div class="credentials">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Enter password" />
+                    </div>
+                    
+                    <button type="login" class="login-button">Login</button>
+                    <p class="forgot-password text-right">
+                        <a href="#"> Forgot password?</a>
+                    </p>
+                </form>
+                <img class = "photo" src = {logo} width = "100" height = "50"/>
+            </div>
+            
+        );
+    }
+}
+
+export default Login
