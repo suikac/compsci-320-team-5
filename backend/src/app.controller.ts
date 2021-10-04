92d3659945a53020068b08879651e94f56b73cf2
ChinhVu320 <chinhvu@umass.edu>
2021-10-04 11:15:11 -0400

    [wip login] need to fix api request

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Inject,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, Query, UseGuards } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
@@ -27,23 +19,14 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('login')
-  async login(
-    @Query('email') email: string,
-    @Query('password') password: string,
+  @Post("api/login")
+  login(
+    @Body('username') username: string,
+    @Body('password') password: string
   ) {
-    console.log('Received login request');
-    const cmd = { cmd: 'login' };
-    const data = { email: email, password: password };
-    const result = this.loginClient.send(cmd, data);
-    try {
-      let response: any = await firstValueFrom(result);
-      return response;
-    } catch (exception) {
-      if (exception.message == 'invalid credentials') {
-        throw new HttpException('invalid credentials', HttpStatus.UNAUTHORIZED);
-      }
-      console.log('Unhandled exception: ' + exception.message);
-    }
+    console.log("Received login request")
+    const cmd = { cmd: "login" }
+    const data = { username: username, password: password }
+    return this.loginClient.send(cmd, data)
   }
 }
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,34 +1,71 @@
 import React, {Component} from "react";
 import logo from "./Logo2.png";
-const [email] = "";
-const [password] = "";
 
 class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {email: "", password: ""}
+
+        this.handleCredentialsChange = this.handleCredentialsChange.bind(this)
+        this.submit_credentials = this.submit_credentials.bind(this)
+    }
+
+    handleCredentialsChange(event) {
+        const type = event.target.type
+        this.setState({
+            [type]: event.value
+        })
+    }
+
     render() {
         return (
             <div class = "container">
-                <form>
+                <form onSubmit={this.submit_credentials}>
                     <h1>Login</h1>
 
                     <div class="credentials">
                         <label>Username</label>
-                        <input type="email" value = {email} class="form-control" placeholder="Enter email" />
+                        <input
+                        type="email"
+                        value={this.state.email}
+                        class="form-control"
+                        onChange={this.handleCredentialsChange}
+                        placeholder="Enter email" />
                     </div>
 
                     <div class="credentials">
                         <label>Password</label>
-                        <input type="password" value = {password} class="form-control" placeholder="Enter password" />
+                        <input
+                        type="password"
+                        value={this.state.password}
+                        class="form-control"
+                        onChange={this.handleCredentialsChange}
+                        placeholder="Enter password" />
                     </div>
-                    
-                    <button type="login" class="login-button">Login</button>
+
+                    <input type="submit" value="Login" class="login-button" />
                     <p class="forgot-password text-right">
                         <a href="#"> Forgot password?</a>
                     </p>
                 </form>
                 <img class = "photo" src = {logo} width = "100" height = "50"/>
             </div>
-            
+
         );
+    }
+
+    async submit_credentials(event) {
+        event.preventDefault()
+
+        const payload = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        const response = await fetch("http://localhost:3000/api/login", {
+            method: "POST",
+            body: JSON.stringify(payload)
+        })
+        console.log(response)
     }
 }
 
