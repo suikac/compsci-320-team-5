134cd0650b7ada3864c7f62b38961cf799e195bc
Mike Huang <yhuang27@uw.edu>
2021-10-05 14:06:58 -0400

    [ add ] front end

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import Login from "./Login"
+import "./App.css"
+
+function App(){
+  return(
+    <div>
+      <Login/>
+    </div>
+  )
+}
+export default App
