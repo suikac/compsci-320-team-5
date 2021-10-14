cc74d36325cd6e4be5c6cd7e4a247c65cf3d0092
Mike Huang <yhuang27@uw.edu>
2021-10-14 13:42:11 -0400

    [ update ] newer schema

--- document/sql/aki.sql
+++ document/sql/aki.sql
@@ -1,31 +1,21 @@
 drop schema if exists aki;
 create schema aki;
 use aki;
-create table resume
-(
-    id   int auto_increment
-        primary key,
-    name varchar(255) not null,
-    file mediumblob   not null
-);
-
 create table employee
 (
-    first_name     varchar(255) null,
-    last_name      varchar(255) not null,
-    email          varchar(255) null,
-    company_name   varchar(255) not null,
-    manager_id     bigint       not null,
+    first_name varchar(255) null,
+    last_name varchar(255) not null,
+    email varchar(255) null,
+    company_name varchar(255) not null,
+    manager_id bigint not null,
     position_title varchar(255) null,
-    start_date     date         null,
-    is_manager     tinyint(1)   null,
-    password       varchar(255) null,
-    resume_id      int          null,
-    id             int auto_increment
+    start_date date null,
+    is_manager tinyint(1) null,
+    password varchar(255) null,
+    resume_id int null,
+    id int auto_increment
         primary key,
-    company_id     int          not null,
-    constraint FK_e7a2cbe5cb8a3776519a4e3a1e7
-        foreign key (resume_id) references resume (id)
+    company_id int not null
 );
 
 create index employee_resume_fk
@@ -33,42 +23,51 @@ create index employee_resume_fk
 
 create table position
 (
-    id                  int auto_increment
+    description longtext null,
+    min_year_experience int null,
+    salary int null,
+    is_posted tinyint(1) default 0 null,
+    title varchar(255) not null,
+    manager_id int null,
+    id int auto_increment
         primary key,
-    description         longtext             null,
-    min_year_experience int                  null,
-    salary              int                  null,
-    is_posted           tinyint(1) default 0 null,
-    title               varchar(255)         not null,
-    manager_id          int                  null,
-    constraint FK_847aa3a3601e72396a0fb03ef5c
+    constraint FK_59d4badb979f73316f8e0a08a02
         foreign key (manager_id) references employee (id)
 );
 
 create index position_employee_fk
     on position (manager_id);
 
+create table resume
+(
+    name varchar(255) not null,
+    file mediumblob not null,
+    id int auto_increment
+        primary key
+);
+
 create table referral
 (
-    id            int auto_increment
+    description longtext not null,
+    referee_name varchar(255) null,
+    is_internal tinyint(1) default 0 null,
+    position_id int not null,
+    resume_id int null,
+    id int auto_increment
         primary key,
-    resume_id     int                  null,
-    referee_email varchar(255)         not null,
-    description   longtext             not null,
-    referee_name  varchar(255)         null,
-    is_internal   tinyint(1) default 0 null,
-    position_id   int                  not null,
-    referee_id    int                  null,
-    referrer_id   int                  not null,
+    referee_email varchar(255) not null,
+    referee_id int null,
+    referrer_id int not null,
     constraint FK_1a4879a67dd4aa7a281c454da5d
         foreign key (resume_id) references resume (id),
-    constraint FK_753fbab8e209904c57c6b529ed5
-        foreign key (referrer_id) references employee (id),
+    constraint FK_3ebf676e9613646800e3749ce65
+        foreign key (referee_id) references employee (id),
     constraint FK_f9624dbcac1a80475b0690baa8d
-        foreign key (position_id) references position (id),
-    constraint referee_employee_fk
-        foreign key (referee_id) references employee (id)
+        foreign key (position_id) references position (id)
 );
+
+create index referee_employee_fk
+    on referral (referee_id);
 
 create index referral_employee_fk
     on referral (referrer_id);
@@ -81,18 +80,18 @@ create index referral_resume_fk
 
 create table tag
 (
-    id          int auto_increment
-        primary key,
-    name        varchar(255) not null,
-    description text         null
+    name varchar(255) not null,
+    description text null,
+    id int auto_increment
+        primary key
 );
 
 create table position_tag
 (
-    id          int auto_increment
+    id int auto_increment
         primary key,
     position_id int not null,
-    tag_id      int not null,
+    tag_id int not null,
     constraint FK_caa1d84e69741ee5540488b585a
         foreign key (tag_id) references tag (id),
     constraint FK_fc6ae90f4c851fed2eef08493ea
@@ -104,5 +103,3 @@ create index position_fk
 
 create index tag_fk
     on position_tag (tag_id);
-
-
