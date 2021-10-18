6e46eaf0bc8203fb6e987f815a58113a0951cffa
yk930608 <yk930608@gmail.com>
2021-10-17 22:11:32 -0400

    [ add ] role guard

--- backend/src/jwt-guard.ts
+++ backend/src/jwt-guard.ts
@@ -21,12 +21,14 @@ export class JwtGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext) {
     const http = context.switchToHttp();
-    const request: any = http.getRequest();
-    let tokenString = request.cookies['AuthToken'];
+    const request: AuthorizedRequest = http.getRequest();
+    let tokenString = request.headers['authorization'];
     console.log('Token received: ' + tokenString);
     if (tokenString == undefined) {
       throw new HttpException('missing credentials', HttpStatus.UNAUTHORIZED);
     }
+
+    tokenString = tokenString.split([' '])[1];
 
     const cmd = { cmd: 'jwt-auth' };
     try {
