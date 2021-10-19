a7be43abc4b944035ac557405b70e05e0caac866
adiracohen <82061451+adiracohen@users.noreply.github.com>
2021-10-19 14:13:00 -0400

    Fixed capitalization in navBar, removed PORT=3002.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import Header from "./components/Header/Header.js";
-import NavBar from "./components/NavBar/NavBar.js";
+import NavBar from "./components/NavBar/navBar.js";
 import Footer from "./components/Footer/Footer.js";
 import Main_Mailbox from "./components/Main_Mailbox/main_mailbox.js";
 import "./App.css"
