4d7a29bbc3b7055e6205257f238f542aae2995f4
yk930608 <yk930608@gmail.com>
2021-10-26 14:17:40 -0400

    [commit] created state to store value and created css for job_posing page

--- frontend/src/components/Job_Posting/Job_posting.js
+++ frontend/src/components/Job_Posting/Job_posting.js
@@ -1,4 +1,24 @@
 import React, {Component} from "react";
 import logo from "./Logo2.png";
 
-class job_posting extends Component{}
+class CreateJobPosting extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {titile: "", salary: 0, minYearsExperience: 0, managerId: 00, tags:[]}
+        defaulttags = ['Git','MySQL','React','Kotlin','Kafka']
+        this.handleCredentialsChange = this.handleCredentialsChange.bind(this)
+    }
+    
+    handleCredentialsChange(event) {
+        const type = event.target.type
+        this.setState({
+            [type]: event.target.value
+        })
+    }
+
+    render(){
+
+    }
+}
+
+export default CreateJobPosting
