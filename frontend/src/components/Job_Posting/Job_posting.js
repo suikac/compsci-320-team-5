a6f3fd38cd9cb92c72a3996b665e7f660fffc53f
yk930608 <yk930608@gmail.com>
2021-10-27 22:23:12 -0400

    [commit] fixed white space

--- frontend/src/components/Job_Posting/Job_posting.js
+++ frontend/src/components/Job_Posting/Job_posting.js
@@ -4,7 +4,7 @@ import styles from "./Job_posting.module.css"
 class CreateJobPosting extends Component{
     constructor(props) {
         super(props)
-        this.state = {titile: "", salary: 0, minYearsExperience: 0, managerId: 0, tags:[],description:""}
+        this.state = {titile: "", salary: "", minYearsExperience: "", managerId: "", tags:"",description:""}
         this.defaulttags = ['Git','MySQL','React','Kotlin','Kafka']
         this.handleCredentialsChange = this.handleCredentialsChange.bind(this)
     }
@@ -20,36 +20,49 @@ class CreateJobPosting extends Component{
         return(
             <div className={styles.container}>
                 <h1>
-                    <div className = {styles.jobTitle}>
+                    <div className = {styles.jobTitleContainer}>
                         <input
                             type="title"
+                            className = {styles.jobTitleText}
                             value = {this.state.titile}
                             onChange={this.handleCredentialsChange}
                             placeholder = "enter job title"
                         />
                     </div>
-                    <div className = {styles.salary}>
+                    <div className = {styles.salaryContainer}>
                         <input
+                            className = {styles.salaryText}
                             type="salary"
                             value = {this.state.salary}
                             onChange={this.handleCredentialsChange}
                             placeholder = "enter job salary"
                         />
                     </div>
-                    <div className = {styles.minYearofExp}>
+                    <div className = {styles.minYearofExpContainer}>
                         <input
+                            className={styles.minYearofExpText}
                             type="minYearsExperience"
                             value = {this.state.minYearsExperience}
                             onChange={this.handleCredentialsChange}
                             placeholder = "enter job minimum year of Experience"
                         />
                     </div>
-                    <div className = {styles.DescriptionBox}>
-                        <input
+                    <div className = {styles.DescriptionContainer}>
+                        <textarea
+                            className = {styles.DescriptionText}
                             type="description"
                             value = {this.state.description}
                             onChange={this.handleCredentialsChange}
                             placeholder = "enter the job description"
+                        />
+                    </div>
+                    <div className = {styles.tagSearchBarContainer}>
+                        <textarea
+                            type="tags"
+                            className = {styles.tagSearchBarText}
+                            value = {this.state.tags}
+                            onChange={this.handleCredentialsChange}
+                            placeholder = "choose job required tags"
                         />
                     </div>
                     <button type ="button"
