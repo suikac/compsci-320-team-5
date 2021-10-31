23a3d7690b1ae831bb719fc668e352675bab5ef6
Mike Huang <yhuang27@uw.edu>
2021-10-31 15:53:13 -0400

    [ add ] mailbox query

--- frontend/src/components/Main_Mailbox/main_mailbox.js
+++ frontend/src/components/Main_Mailbox/main_mailbox.js
@@ -2,8 +2,14 @@ import React, {Component} from "react";
 import Email_Bar from "./email_bar";
 import "./main_mailbox.module.css";
 import main_mailboxcss from "./main_mailbox.module.css";
+import { apiGet, apiPost } from '../../utils/api-fetch';
 
-function Main_Mailbox() {
+
+let referrals = []
+
+async function Main_Mailbox() {
+  await getUnreadReferral()
+  console.log(referrals['0']['position']['title'])
     return (
         <ul className={main_mailboxcss.Mailbox}>
             <li><Email_Bar/></li>
@@ -18,6 +24,11 @@ function Main_Mailbox() {
             <li><Email_Bar/></li>
         </ul>
     );
+}
+
+async function getUnreadReferral() {
+  const data = await apiGet("/referral/get?isRead=0")
+  data.json().then(res => referrals = res)
 }
 
 export default Main_Mailbox
