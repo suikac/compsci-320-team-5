2db21d5749c2e7e0f3cfec8fd7bb043b883f0010
yk930608 <yk930608@gmail.com>
2021-11-01 17:07:45 -0400

    [commit] fixed css

--- frontend/src/components/Job_Posting/Job_posting.js
+++ frontend/src/components/Job_Posting/Job_posting.js
@@ -18,56 +18,57 @@ class CreateJobPosting extends Component{
 
     render(){
         return(
-            <div className={styles.grid}>
-                    <div className = {styles.jobTitleContainer}>
-                        <input
-                            type="title"
-                            className = {styles.jobTitleText}
-                            value = {this.state.titile}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "enter job title"
-                        />
-                    </div>
-                    <div className = {styles.salaryContainer}>
-                        <input
-                            className = {styles.salaryText}
-                            type="salary"
-                            value = {this.state.salary}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "enter job salary"
-                        />
-                    </div>
-                    <div className = {styles.minYearofExpContainer}>
-                        <input
-                            className={styles.minYearofExpText}
-                            type="minYearsExperience"
-                            value = {this.state.minYearsExperience}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "enter job minimum year of Experience"
-                        />
-                    </div>
-                    <div className = {styles.DescriptionContainer}>
-                        <textarea
-                            className = {styles.DescriptionText}
-                            type="description"
-                            value = {this.state.description}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "enter the job description"
-                        />
-                    </div>
-                    <div className = {styles.tagSearchBarContainer}>
-                        <textarea
-                            type="tags"
-                            className = {styles.tagSearchBarText}
-                            value = {this.state.tags}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "choose job required tags"
-                        />
-                    </div>
-                    <button type ="button"
-                    onClick = {this.submit_credentials}
-                    className = {styles.createButton}>Create Job
-                    </button>
+            <div className = {styles.container}>
+                <div className = {styles.jobTitleContainer}>
+                    <input
+                        type="title"
+                        className = {styles.jobTitleText}
+                        value = {this.state.titile}
+                        onChange={this.handleCredentialsChange}
+                        placeholder = "enter job title"
+                    />
+                </div>
+                <div className = {styles.salaryContainer}>
+                    <input
+                        className = {styles.salaryText}
+                        type="salary"
+                        value = {this.state.salary}
+                        onChange={this.handleCredentialsChange}
+                        placeholder = "enter job salary"
+                    />
+                </div>
+                <div className = {styles.minYearofExpContainer}>
+                    <input
+                        className={styles.minYearofExpText}
+                        type="minYearsExperience"
+                        value = {this.state.minYearsExperience}
+                        onChange={this.handleCredentialsChange}
+                        placeholder = "enter job minimum year of Experience"
+                    />
+                </div>
+                <div className = {styles.tagSearchBarContainer}>
+                    <textarea
+                        type="tags"
+                        className = {styles.tagSearchBarText}
+                        value = {this.state.tags}
+                        onChange={this.handleCredentialsChange}
+                        placeholder = "choose job required tags"
+                    />
+                </div>
+                <button 
+                type ="button"
+                onClick = {this.submit_credentials}
+                className = {styles.createButton}>Create Job
+                </button>
+                <div className = {styles.DescriptionContainer}>
+                    <textarea
+                        className = {styles.DescriptionText}
+                        type="description"
+                        value = {this.state.description}
+                        onChange={this.handleCredentialsChange}
+                        placeholder = "enter the job description"
+                    />
+                </div>
             </div>
         );
     }
