8481acf37260c5eb51eaea41a5b0f63276c09ae3
ChinhVu320 <chinhvu@umass.edu>
2021-11-03 12:02:38 -0400

    Use Stack for Header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,33 +4,29 @@ import NotifPic from "./Notif.png";
 import ProfPic from "./ProfPic.png";
 import searchicon from "./searchicon.png"
 import headercss from "./Header.module.css";
-import {DropdownButton, Dropdown} from "react-bootstrap"
+import {DropdownButton, Dropdown, Stack} from "react-bootstrap"
 
 class Header extends Component {
     render() {
        return (
-        <div className = {headercss.HeaderContainer}>
-            <div>
-                <img className ={headercss.notifpic} src={NotifPic} alt="NotifPic"></img>
-                <div className ={headercss.profpic}>
-                        <Dropdown>
-                            <Dropdown.Toggle variant='default' bsPrefix='p-0'>
-                                <img src= {ProfPic}></img>
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
+           <div className = {headercss.HeaderContainer}>
+            <Stack direction='horizontal'>
+                {/* <div className={headercss.logoContainer}> */}
+                <img className = {headercss.logoimg} src = {logo} alt = "A logo"></img>
+                {/* </div> */}
                 <h1> Insert Company logo here from the backend </h1>
-            </div>
-                <div className ={headercss.logocontainer}>
-                    <img className = {headercss.logoimg} src = {logo} alt = "A logo"></img>
-                </div>
-            </div>
-        </div>
-
+                <Dropdown>
+                    <Dropdown.Toggle variant='default' bsPrefix='p-0'>
+                        <img src= {ProfPic} className ={headercss.profpic}></img>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Stack>
+           </div>
         )
     }
 }
