ca8abc84155f57c6f5cefebf9ea9ddbe980bb332
James Topa <jtopa@gmail.com>
2021-11-07 16:59:43 -0500

    Preparing for backend connection

--- frontend/src/components/Main_Home/main_home.js
+++ frontend/src/components/Main_Home/main_home.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import Footer from "../Footer/Footer";
 import maincss from "./main_home.module.css";
 
-function Main_Home(){
-  return(
-      <div className = {maincss.MainContainer}>
-          <h1 class = "rec"> RECOMMENDED</h1>
-          <div className = {maincss.MainJobContainer}>
-              <h1 class = "jobTitle"> Job Title</h1>
-              <h1 class = "managerInfo"> Manager Info</h1>
-              <h1 class = "tags"> Tags</h1>
-              <div className = {maincss.refButton}>
-                <input type="submit" value="REFER" class="refer-button" />
-              </div>
-          </div>
-          <Footer />
-      </div>
-  )
+class Main_Home extends Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+    
+}
+
+handleCredentialsChange(event) {
+    const type = event.target.type
+    this.setState({
+        [type]: event.target.value
+    })
+}
+render(){
+    return(
+        <div className = {maincss.MainContainer}>
+            <h1 class = "rec"> RECOMMENDED</h1>
+            <div className = {maincss.MainJobContainer}>
+                <h1 class = "jobTitle"> Job Title</h1>
+                <h1 class = "managerInfo"> Manager Info</h1>
+                <h1 class = "tags"> Tags</h1>
+                <div className = {maincss.refButton}>
+                  <input type="submit" value="REFER" class="refer-button" />
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+  }
 }
 
 export default Main_Home
