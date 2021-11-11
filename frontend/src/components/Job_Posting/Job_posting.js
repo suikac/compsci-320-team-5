f6b081c65321e87daee61d2609861473b9008806
yk930608 <yk930608@gmail.com>
2021-11-11 00:24:08 -0500

    [connected] defaulttags connectted with backend

--- frontend/src/components/Job_Posting/Job_posting.js
+++ frontend/src/components/Job_Posting/Job_posting.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState } from "react";
 import styles from "./Job_posting.module.css"
 import { apiPost } from "../../utils/api-fetch"
 import{JobCreateSuccessedPopUp} from "./Job_postingPopUp"
@@ -8,7 +8,8 @@ class CreateJobPosting extends Component{
     constructor(props) {
         super(props)
         this.state = {title: "", salary: "", minYearsExperience: "", tags:[], searchBarTag: "",
-        description:"",createJobSuccess:false}
+        description:"",createJobSuccess:false,isLoaded:false,defaultTag:[]}
+        this.defaultTags = ['Git','hahaha','hahahahaha']
         this.tagFromDB = []
         this.submit_credentials = this.submit_credentials.bind(this)
         this.handleCredentialsChange = this.handleCredentialsChange.bind(this)
@@ -39,104 +40,108 @@ class CreateJobPosting extends Component{
             searchBarTag:''
         })
     }
-
-    render(){
-        const defaulttags = []
-        this.getTags()
-        return(
-            <form>
-                <h2 className={styles.h2}>
-                    <p className={styles.p}> Job Creating </p>
-                    <div className = {styles.jobTitleContainer}>
-                        <input
-                            name='title'
-                            type="text"
-                            value = {this.state.title}
-                            className = {styles.jobTitleText}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "Job Title"
-                        >
-                        </input>
-                    </div>
-                    <div className = {styles.salaryContainer}>
-                        <input
-                            name = 'salary'
-                            type = 'number'
-                            className = {styles.salaryText}
-                            value = {this.state.salary}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "Job Salary"
-                        />
-                    </div>
-                    <div className = {styles.minYearofExpContainer}>
-                        <input
-                            name = 'minYearsExperience'
-                            type = 'number'
-                            className={styles.minYearofExpText}
-                            value = {this.state.minYearsExperience}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "Minimum Year of Experience"
-                        />
-                    </div>
-
-                    <div className = {styles.tagSearchBarContainer}>
-                        <input  list="brow"
-                                name = 'searchBarTag'
-                                type = 'text'
-                                value = {this.state.searchBarTag}
+    render(){  
+        this.setTagsArray()
+        if(!this.state.isLoaded){
+            return <div>loading</div>
+        }
+        else{
+            return(
+                <form>
+                    <h2 className={styles.h2}>
+                        <p className={styles.p}> Job Creating </p>
+                        <div className = {styles.jobTitleContainer}>
+                            <input
+                                name='title'
+                                type="text"
+                                value = {this.state.title}
+                                className = {styles.jobTitleText}
                                 onChange={this.handleCredentialsChange}
-                                placeholder = "Search Tag"
-                                className = {styles.tagSearchBarText}/>
-                            <datalist id="brow">
-                            {defaulttags.map(tags => (
-                                <option>{tags}</option>
-                            ))}
-                            </datalist>
-                            {this.state.searchBarTag != ''?
-                            <button type = 'button'
-                                    className = {styles.addButton}
-                                    onClick = {this.addValueToTag}
-                                    >add</button>:
-                                    <button className = {styles.disabledaddButton}>add</button>
-                            }
-                        <div className = {styles.tagStoreContainer}>
-                            {
-                                this.state.tags.map(tag => (
-                                    <div>{tag}</div>
-                                ))
-                            }
+                                placeholder = "Job Title"
+                            >
+                            </input>
                         </div>
-                    </div>
-                    <div className = {styles.DescriptionContainer}>
-                        <textarea required
-                            type= 'text'
-                            name = 'description'
-                            className = {styles.DescriptionText}
-                            value = {this.state.description}
-                            onChange={this.handleCredentialsChange}
-                            placeholder = "Job Description"
-                        />
-                    </div>
-                    {this.state.title != "" && this.state.description !=""?
-                            <button
-                            type = 'button'
-                            onClick = {this.submit_credentials}
-                            className = {styles.createButton}>Create</button>
-                            :
-                            <button
-                            type = 'button'
-                            disabled
-                            className = {styles.disabledcreateButton}>Create
-                            </button>
-                    }
-                    <JobCreateSuccessedPopUp trigger = {this.state.createJobSuccess}
-                    effect = {() => setTimeout(() => this.setState({
-                        createJobSuccess: false}), 3000)}>
-                    </JobCreateSuccessedPopUp>
-                </h2>
-            </form>
-        );
+                        <div className = {styles.salaryContainer}>
+                            <input
+                                name = 'salary'
+                                type = 'number'
+                                className = {styles.salaryText}
+                                value = {this.state.salary}
+                                onChange={this.handleCredentialsChange}
+                                placeholder = "Job Salary"
+                            />
+                        </div>
+                        <div className = {styles.minYearofExpContainer}>
+                            <input
+                                name = 'minYearsExperience'
+                                type = 'number'
+                                className={styles.minYearofExpText}
+                                value = {this.state.minYearsExperience}
+                                onChange={this.handleCredentialsChange}
+                                placeholder = "Minimum Year of Experience"
+                            />
+                        </div>
+
+                        <div className = {styles.tagSearchBarContainer}>
+                            <input  list="brow"
+                                    name = 'searchBarTag'
+                                    type = 'text'
+                                    value = {this.state.searchBarTag}
+                                    onChange={this.handleCredentialsChange}
+                                    placeholder = "Search Tag"
+                                    className = {styles.tagSearchBarText}/>
+                                <datalist id="brow">
+                                {this.state.defaultTag.map(tags => (
+                                    <option>{tags}</option>
+                                ))}
+                                </datalist>
+                                {this.state.searchBarTag != ''?
+                                <button type = 'button'
+                                        className = {styles.addButton}
+                                        onClick = {this.addValueToTag}
+                                        >add</button>:
+                                        <button className = {styles.disabledaddButton}>add</button>
+                                }
+                            <div className = {styles.tagStoreContainer}>
+                                {
+                                    this.state.tags.map(tag => (
+                                        <div>{tag}</div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                        <div className = {styles.DescriptionContainer}>
+                            <textarea required
+                                type= 'text'
+                                name = 'description'
+                                className = {styles.DescriptionText}
+                                value = {this.state.description}
+                                onChange={this.handleCredentialsChange}
+                                placeholder = "Job Description"
+                            />
+                        </div>
+                        {this.state.title != "" && this.state.description !=""?
+                                <button
+                                type = 'button'
+                                onClick = {this.submit_credentials}
+                                className = {styles.createButton}>Create</button>
+                                :
+                                <button
+                                type = 'button'
+                                disabled
+                                className = {styles.disabledcreateButton}>Create
+                                </button>
+                        }
+                        <JobCreateSuccessedPopUp trigger = {this.state.createJobSuccess}
+                        effect = {() => setTimeout(() => this.setState({
+                            createJobSuccess: false}), 3000)}>
+                        </JobCreateSuccessedPopUp>
+                    </h2>
+                </form>
+            );
+        }
     }
+    //}
     async submit_credentials() {
         const payload = {
             tags:this.tags,
@@ -156,7 +161,21 @@ class CreateJobPosting extends Component{
     }
     async getTags() {
         const a = await apiPost('/tag/get')
-        return a.json()
+        return a
+    }
+    setTagsArray(){
+        this.getTags().then(a => a.json()).then( a =>{
+            this.setState({
+                        defaultTag:a.map(x => x.name),
+                        isLoaded:true
+                    })
+        })
+        // .then(a=>{
+        //     this.setState({
+        //         defaultTag:a,
+        //         isLoaded:true
+        //     })
+        // })
     }
 }
 
