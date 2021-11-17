c49b93e802e4ccfc9330ae17b1d0bedbc90abe0c
yk930608 <yk930608@gmail.com>
2021-11-16 23:12:10 -0500

    Finshed Tag Search Bar Working on Tag Store Bar

--- frontend/src/components/Filter/TagsSearchBar.js
+++ frontend/src/components/Filter/TagsSearchBar.js
@@ -1,17 +1,17 @@
-import React, {Component, useEffect, useState } from "react";
+import React, {useRef, useEffect, useState } from "react";
 import { apiPost } from "../../utils/api-fetch"
 
 function TagsSearchBar(props) {
     const [curTag,setCurTag] = useState('')
-    const [selectedTags,setSeletedTag] = useState([])
+    const [selectedTags,setSeletedTags] = useState([])
     const [isLoaded,setLoad] = useState(false)
     const [deafaultTags,setDefaultTags] = useState([])
-
+    const target = useRef(null)
     async function getTags() {
         const a = await apiPost('/tag/get')
         return a
     }
-
+    
     useEffect(()=>{
         let temp = []
         getTags().then(a => a.json()).then(a =>{
@@ -19,13 +19,36 @@ function TagsSearchBar(props) {
         })
     },[])
 
+    function _onKeyDown(e){
+       // if(e.key == 'Enter'){
+            let temp = [...selectedTags]
+            temp.push(curTag)
+            console.log(temp)
+            // setSeletedTags(temp)
+            // console.log(selectedTags)
+        //}
+    }
 
     return(
         <div>
-            <input list="brow"/>
-                <datalist id="brow">
+            <input list="brow" 
+                    onKeyDown = {curTag != '' ? async (e) => {
+                        if(e.key == 'Enter'){
+                            let temp = [...selectedTags]
+                            temp.push(curTag)
+                            setSeletedTags(temp)
+                            props.fun(selectedTags)
+                        }
+                      }:undefined}
+                    value = {curTag}
+                    onChange = {async (e) => {
+                        setCurTag(e.target.value)
+                        // console.log(curTag)
+                      }}
+            />
+                <datalist id="brow" >
                 {deafaultTags.map(tags => (
-                    <option>{tags}</option>
+                    <option key = {tags} >{tags}</option>
                 ))}
                 </datalist>
         </div>
