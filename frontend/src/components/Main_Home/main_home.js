f105d03d87f9370c88eaec6effd46b038d6dba7f
James Topa <jtopa@gmail.com>
2021-11-30 12:24:51 -0500

    Updated the homepage to more resemble figma

--- frontend/src/components/Main_Home/main_home.js
+++ frontend/src/components/Main_Home/main_home.js
@@ -2,6 +2,8 @@ import React, { Component, useEffect, useState } from 'react';
 import Footer from "../Footer/Footer";
 import maincss from "./main_home.module.css";
 import { apiGet,apiPost } from '../../utils/api-fetch';
+import left from './left_arrow.png'
+import right from './right_arrow.png'
 
 function Main_Home(){
 
@@ -27,8 +29,34 @@ function Main_Home(){
   if(index>4) {
     index = 0
   }
-    
-  
+  const titleStyle = {
+    fontSize: 20,
+    color: "black",
+    textAlign: "right",
+    paddingTop: "30px",
+    paddingRight:"15px",
+  }
+  const managerStyle = {
+    fontSize: 20,
+    color: "black",
+    textAlign: "center",
+    paddingTop: "30px",
+
+  }
+  const tagsStyle = {
+    fontSize: 20,
+    color: "black",
+    textAlign: "left", 
+    paddingTop: "30px",
+    paddingLeft:"15px",
+  }
+
+  const recStyle = {
+    color: "gray",
+
+  }
+
+
   if (!isLoad){
     return <div>Loading...</div>;
   } else {
@@ -36,14 +64,15 @@ function Main_Home(){
     
     return(
       <div className={maincss.MainContainer}>
-        <h1 className="rec"> RECOMMENDED</h1>
+        <h1 style = {recStyle}> RECOMMENDED</h1>
 
         <div className={maincss.MainJobContainer}>
-          <button onClick={incrementIndex}>Next</button>
-          <button onClick={decrementIndex}> Prev </button>
-          <h2 className="jobTitle">Job Title: {positionData[index].title} </h2>
-          <h2 className="managerInfo">Manager:  {positionData[index].manager.firstName} {positionData[index].manager.lastName}</h2>
-          <h2 className="tags">Tags: {positionData[index].tags.join(', ')}</h2>
+          <button type="prev-next"><img src={left} onClick={decrementIndex}/></button>
+          <button type="prev-next"><img src={right} onClick={incrementIndex}/></button>
+          
+          <h2 style = {titleStyle}>Job Title: {positionData[index].title} </h2>
+          <h2 style = {managerStyle}>Manager:  {positionData[index].manager.firstName} {positionData[index].manager.lastName}</h2>
+          <h2 style = {tagsStyle}>Tags: {positionData[index].tags.join(', ')}</h2>
           <div className={maincss.refButton}>
             <input type="submit" value="REFER" className="refer-button" />
           </div>
