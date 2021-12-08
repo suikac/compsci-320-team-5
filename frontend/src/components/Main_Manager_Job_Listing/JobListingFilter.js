7bc54c3328f39e62a861c7497d510e383bec21dd
AVS1508 <aditya.11.present@gmail.com>
2021-12-08 11:13:03 -0500

    Prepared JobListingFilter for Manage Job Referrals view

---
+++ frontend/src/components/Main_Manager_Job_Listing/JobListingFilter.js
@@ -0,0 +1,62 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { clearFilter, useFilter, useFilterParam } from "../Filter/Filter"
+import FilterInput from "../Filter/FilterInput"
+import * as styles from "./JobListingFilter.module.css"
+import TagsSearchBar from "../Filter/TagsSearchBar"
+import TagStore from "../Filter/TagStore"
+
+function JobListingFilter(props) {
+  const filter = useFilter('/position/get?cur=1', props.setResult, props.pageLoadTrigger)
+  const [minSalary, setMinSalary] = useFilterParam("", 'minSalary', filter)
+  const [maxSalary, setMaxSalary] = useFilterParam("", 'maxSalary', filter)
+  const [jobTitle, setJobtitle] = useFilterParam("", 'title', filter)
+  const [tags, setTags] = useFilterParam([], 'tags', filter)
+
+  const clearForm = (filter) => {
+    clearFilter(filter);
+    setMinSalary("");
+    setMaxSalary("");
+    setJobtitle("");
+    setTags([]);
+  }
+
+
+  return (
+    <Container>
+      <Row className={[styles.menu, 'py-1']} xs='auto'>
+        <Col>Filter Job Listings</Col>
+      </Row>
+      <Row className={[styles.row, 'py-1']} xs='auto' style={{justifyContent: "left"}}>
+        <Col style={{display: "flex", flexDirection: "column"}}>
+          <Row className={[styles.row, 'py-1', '']} xs='auto'>
+            <Col>
+              <label for="Job Title"> Job Title </label>&nbsp;
+              <FilterInput value={jobTitle} onChange={setJobtitle} id="Job Title"/>
+            </Col>
+            <Col>
+              <label for="Salary Range"> Salary Range </label>&nbsp;
+              <FilterInput value={minSalary} onChange={setMinSalary} id="Salary Range"/>
+              <span>&nbsp; - &nbsp;</span>
+              <FilterInput value={maxSalary} onChange={setMaxSalary} id="Salary Range"/>
+            </Col>
+          </Row>
+          <Row className={[styles.row, 'py-1']} xs='auto'>
+            <Col>
+              <label for="Tag"> Tags </label>&nbsp;
+              <TagsSearchBar fun = {setTags} tags = {tags} id="Tag" style={{width: "100%"}}/>
+              <TagStore fun = {setTags} tags = {tags} />
+            </Col>
+          </Row>
+        </Col>
+        <Col style={{justifyContent: "center", alignItems: "center", paddingLeft: "3em"}}>
+          <button className={styles.clearButton}
+          onClick={() => clearForm(filter)}>
+            Clear
+          </button>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default JobListingFilter
