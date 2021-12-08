fc9997d381cc83f2044bfb63bd7eb3348b9212ed
ChinhVu320 <chinhvu@umass.edu>
2021-12-07 23:37:34 -0500

    Add buttons to filter

--- frontend/src/components/Main_Refer/JobListingFilter.js
+++ frontend/src/components/Main_Refer/JobListingFilter.js
@@ -13,6 +13,7 @@ function JobListingFilter(props) {
   const filter = useFilter('/position/get', props.setResult, props.pageLoadTrigger)
   const [minSalary, setMinSalary] = useFilterParam("", 'minSalary', filter)
   const [maxSalary, setMaxSalary] = useFilterParam("", 'maxSalary', filter)
+  const [minYearExp, setMinYearExp] = useFilterParam("", 'minYearExperience', filter)
   const [jobTitle, setJobtitle] = useFilterParam("", 'title', filter)
   const [managerName, setManagerName] = useFilterParam("", 'managerName', filter)
   const [managerEmail, setManagerEmail] = useFilterParam("", 'managerEmail', filter)
@@ -41,25 +42,30 @@ function JobListingFilter(props) {
       <>
       <Row className={[styles.row, 'py-1']} xs='auto'>
         <Col>
-          <label for="Job Title"> Job Title </label> <FilterInput value={jobTitle} onChange={setJobtitle} id="Job Title"/>
+          <ParamField label='Job Title' value={jobTitle} onChange={setJobtitle} id={styles.jobTitle} />
         </Col>
         <Col>
-          <label for="Minimum Salary"> Minimum Salary </label>
-          <FilterInput value={minSalary} onChange={setMinSalary} id="Minimum Salary"/>
-          <label for="Maximum Salary"> Maximum Salary </label>
-          <FilterInput value={maxSalary} onChange={setMaxSalary} id="Maximum Salary"/>
+          <ParamField label='Minimum Years of Experience' value={minYearExp} onChange={setMinYearExp} id="minYearExp"/>
+        </Col>
+      </Row>
+      <Row className={[styles.row, 'py-1']} xs='auto'>
+        <Col>
+          <ParamField label='Minimum Salary' value={minSalary} onChange={setMinSalary} id={styles.minSalary}/>
+          <ParamField label='Maximum Salary' value={maxSalary} onChange={setMaxSalary} id={styles.maxSalary}/>
         </Col>
         <Col>
-          <button className={styles.clearButton}
-          onClick={() => clearFilter(filter)}>
-            Clear
+          <button
+            aria-label='Clear all filter fields'
+            className={styles.clearButton}
+            onClick={() => clearFilter(filter)}>
+            Clear All
           </button>
         </Col>
       </Row>
       <Row className={[styles.row, 'py-1']} xs='auto'>
         <Col>
           <label for="Tag"> Tags </label>
-          <TagsSearchBar fun = {setTags} tags = {tags} id="Tag" />
+          <TagsSearchBar fun = {setTags} tags = {tags} id="Tag" addButtonClass={styles.clearFieldButton}/>
           <TagStore fun = {setTags} tags = {tags} />
         </Col>
       </Row>
@@ -68,22 +74,37 @@ function JobListingFilter(props) {
       <>
       <Row className={[styles.row, 'py-1']} xs='auto'>
         <Col>
-          <label for="Name"> Name </label> <FilterInput value={managerName} onChange={setManagerName} id="Name"/></Col>
+          <ParamField label="Manager's Name" value={managerName} onChange={setManagerName} id={styles.managerName}/></Col>
         <Col>
-          <button className={styles.clearButton}
-          onClick={() => clearFilter(filter)}>
-            Clear
+          <button
+            aria-label='Clear all filter fields'
+            className={styles.clearButton}
+            onClick={() => clearFilter(filter)}>
+            Clear All
           </button>
         </Col>
       </Row>
       <Row>
         <Col className={[styles.row, 'py-1']} xs='auto'>
-          Email <FilterInput value={managerEmail} onChange={setManagerEmail} />
+          <ParamField label="Manager's Email" value={managerEmail} onChange={setManagerEmail} id={styles.managerEmail}/>
         </Col>
       </Row>
       </>
       }
     </Container>
+  )
+}
+
+function ParamField(props) {
+  return (
+    <>
+      <label for={props.id}> {props.label} </label>
+      <FilterInput {...props} className={styles.filterInput}/>
+      <button
+        aria-label={'Clear ' + props.label}
+        className={styles.clearFieldButton}
+        id={props.id + 'clearButton'}>x</button>
+    </>
   )
 }
 
