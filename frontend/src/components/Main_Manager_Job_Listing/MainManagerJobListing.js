48b3a71582d0ba233256d9fd4ccd55de6522adef
ChinhVu320 <chinhvu@umass.edu>
2021-12-08 13:48:55 -0500

    Fix scrolling for manager listing view

--- frontend/src/components/Main_Manager_Job_Listing/MainManagerJobListing.js
+++ frontend/src/components/Main_Manager_Job_Listing/MainManagerJobListing.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import PositionItem from "./PositionItem";
-import ReferCSS from "./MainManagerJobListing.module.css"
+import styles from "./MainManagerJobListing.module.css"
 import JobListingFilter from "./JobListingFilter";
 import { usePageLoadTrigger } from "../Filter/Filter";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -10,26 +10,26 @@ const MainManagerJobListing = () => {
     const [trigger, loadPage] = usePageLoadTrigger()
 
     return (
-        <React.Fragment>
+        <div className={styles.container}>
             {/* Component used for filtering position data */}
             <JobListingFilter setResult={setData} pageLoadTrigger={trigger}/>
             {/* Component that displays position data with PositionItem components */}
-            <div className={'mt-1'} id={ReferCSS.scrollableTarget}>
-            <InfiniteScroll
-            dataLength={data.length}
-            hasMore={true}
-            next={loadPage}
-            scrollableTarget={ReferCSS.scrollableTarget}>
-                {data.map((e, index) => {
-                    return (
-                        <div key={index} className={`mb-2 ${ReferCSS.positionItemContainer}`}> 
-                            <PositionItem key={e.id} {...e} />
-                        </div>
-                    );
-                })}
-            </InfiniteScroll>
+            <div className={'mt-1'} id={styles.scrollableTarget}>
+                <InfiniteScroll
+                dataLength={data.length}
+                hasMore={true}
+                next={loadPage}
+                scrollableTarget={styles.scrollableTarget}>
+                    {data.map((e, index) => {
+                        return (
+                            <div key={index} className={`mb-2 ${styles.positionItemContainer}`}>
+                                <PositionItem key={e.id} {...e} />
+                            </div>
+                        );
+                    })}
+                </InfiniteScroll>
             </div>
-        </React.Fragment>
+        </div>
     )
 }
 
