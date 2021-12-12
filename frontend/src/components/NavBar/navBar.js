5de3de59e2fb8d4f826fcac9357098560eaed79d
AVS1508 <aditya.11.present@gmail.com>
2021-12-11 22:54:00 -0500

    Changed the Navbar Item to be "PREVIOUS REFERRALS"; Formatted navBar.js correctly

--- frontend/src/components/NavBar/navBar.js
+++ frontend/src/components/NavBar/navBar.js
@@ -1,54 +1,49 @@
 import React, { Component } from "react"
 import NavBarcss from "./navBar.module.css";
-import {Row, Col} from "react-bootstrap"
 import { Link as RLink, useLocation } from 'react-router-dom'
 import * as paths from "../../utils/paths"
 import ArrowImg from './arrow.png'
 
 class NavBar extends Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() {
-        return(
-                <ul className={NavBarcss.NavBarContainer}>
-                        <Link to='/'>
-                            HOME
-                        </Link>
-                        <Link to={paths.REFER}>
-                            REFER
-                        </Link>
-                        <Link to={paths.PREV_REF}>
-                            PREVIOUS REF
-                        </Link>
-                    {this.props.isManager ?
-                        <Link to={paths.CREATE_POSTING}>
-                            CREATE POSTING
-                        </Link>
+        return (
+            <ul className={NavBarcss.NavBarContainer}>
+                <Link to='/'>
+                    HOME
+                </Link>
+                <Link to={paths.REFER}>
+                    REFER
+                </Link>
+                <Link to={paths.PREV_REF}>
+                    PREVIOUS REFERRALS
+                </Link>
+                {this.props.isManager ?
+                    <Link to={paths.CREATE_POSTING}>
+                        CREATE POSTING
+                    </Link>
                     :
                     null
-                    }
-                    {this.props.isManager ?
-                        <Link to={paths.MANAGER_JOB_LISTING}>
-                            MANAGE JOB REFERRALS
-                        </Link>
+                }
+                {this.props.isManager ?
+                    <Link to={paths.MANAGER_JOB_LISTING}>
+                        MANAGE JOB REFERRALS
+                    </Link>
                     :
                     null
-                    }
-                </ul>
+                }
+            </ul>
         )
     }
 }
 
 function Link(props) {
     const location = useLocation()
-    const {children, to, ..._props} = props
+    const { children, to } = props
     return (
         <li>
             <RLink to={to} className={location.pathname === to ? NavBarcss.selected : ''}>
                 {children}
-                <img src={ArrowImg} alt={"Navbar Arrow"}/>
+                <img src={ArrowImg} alt={"Navbar Arrow"} />
             </RLink>
         </li>
     )
