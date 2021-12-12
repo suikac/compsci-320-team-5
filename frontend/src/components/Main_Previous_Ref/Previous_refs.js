a7b913274769a7d0e9ac4b181978a8632f6a8ab5
AVS1508 <aditya.11.present@gmail.com>
2021-12-12 11:16:01 -0500

    Updated the previous referrals page to display distinction between referrals

--- frontend/src/components/Main_Previous_Ref/Previous_refs.js
+++ frontend/src/components/Main_Previous_Ref/Previous_refs.js
@@ -4,25 +4,25 @@ import MPRcss from "./main_previous_ref.module.css";
 function RefBox(props){
     return (
         <div className = {MPRcss.refBoxCont}>
-            <div className="container mt-5">
+            <div className="container">
                 <div className="row">
                     <h2>{props.refTitle}</h2> {/*Referral Title*/}
                     <div className="col-sm-4">
-                        <div style = {{border: "1px solid gray", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}><h4>Name: {props.referred}</h4></div>
+                        <div style = {{border: "2px solid #197278", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}><h4>Name: {props.referred}</h4></div>
                          {/*Person Referred*/}
-                         <div style = {{border: "1px solid gray", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}>
+                         <div style = {{border: "2px solid #197278", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}>
                             <h4>About Position:</h4>
                             <p>{props.jobDesc}</p>
                          </div>
                     </div>
                     <div className="col-sm-4">
-                        <div style = {{border: "1px solid gray", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}>
+                        <div style = {{border: "2px solid #197278", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}>
                             <h4>Minimum Year Experience: <h5>{props.minExp}</h5></h4>
                         </div>
-                        <div style = {{border: "1px solid gray", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}><h4>Offered Salary: <h5>{props.salary}</h5></h4></div>
+                        <div style = {{border: "2px solid #197278", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}><h4>Offered Salary: <h5>{props.salary}</h5></h4></div>
                     </div>
                     <div className="col-sm-4">
-                        <div style = {{border: "1px solid gray", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}>
+                        <div style = {{border: "2px solid #197278", borderRadius: "1em", padding: "0.5em", margin: ".5em"}}>
                             <h4>Referred Description: </h4>
                             <p>{props.refDesc}</p>
                         </div>
